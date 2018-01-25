fd2edd08d907d41320bc513ec37e8ec784f2d36b
pikachu007 <pikachu007_25@hotmail.com>
2018-01-25 10:45:20 -0600

    extraccion de secuecias terminada

--- Arbol.py
+++ Arbol.py
@@ -6,7 +6,8 @@ nombre_archivo="prueba.txt"
 n_raiz=Nodo(0,0,0,0)    #Creacion del nodo Raiz
 n_actual=n_raiz
 d_nodos={}          #creacion de diccionario de nodos
-
+secuencia=[]   #Lista con la secuencia de elementos
+conteoMax=0     #la mayor cantidad de veces que se ha visitado un Nodo
 archivo = open(nombre_archivo, "r")     #lectura del archivo
 for linea in archivo.readlines():
     linea=linea.split(",")
@@ -28,7 +29,19 @@ for linea in archivo.readlines():
         n_existente=d_nodos[id_nodo]
         n_actual.siguiente_nodo( n_existente)  # enlace de nodo existete
         n_actual=n_existente
-        n_actual.cuenta()
+        n_actual.cuenta()       #Al existir el nodo, se lleva un conteo de las veces que ha sido usado
+        #print(n_actual.getCuenta())
+        if n_actual.getCuenta()>= conteoMax:
+            conteoMax=n_actual.getCuenta()      #actualizacion del conteoMaximo
+
+        if ((conteoMax*0.75)<=n_actual.getCuenta()) and (secuencia.count(n_actual)==0) and (conteoMax>=90):     #extraccion de la secuencia
+            secuencia.append(n_actual)
+        else:
+            if len(secuencia)>1:
+                for nodo in secuencia:
+                    print (nodo.getAccion(),nodo.getColocacion(),nodo.getCuenta())
+            #print ("")
+            secuencia=[]
     else:
         d_nodos[id_nodo]=n_nuevo
         n_actual.siguiente_nodo(n_nuevo)    # enlace de nodo nuevo
