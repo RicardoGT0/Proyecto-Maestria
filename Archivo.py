7dd3a2a4c130988cc189be6a7274d18ff9959963
pikachu007 <pikachu007_25@hotmail.com>
2018-01-28 16:56:44 -0600

    creacion de modulo para guardar registro de acciones

---
+++ Archivo.py
@@ -0,0 +1,12 @@
+from time import time
+
+t_inicial=time()
+
+def escribir(dispositivo, accion, t_final, colocacion):
+    global t_inicial
+    n_archivo="lista_acciones.txt"
+    archivo=open(n_archivo,"a")
+    tiempo=round(t_final-t_inicial,2)
+    archivo.write(dispositivo + "," + accion + "," + str(tiempo) + "," + colocacion + "\n")
+    archivo.close()
+    t_inicial = time()
