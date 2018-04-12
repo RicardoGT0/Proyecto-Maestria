8171cfda531269e97e1154ce5ac30d9df9e757e7
pikachu007 <pikachu007_25@hotmail.com>
2018-04-11 20:09:27 -0500

    Primer implementacioin
    Arbol en memoria, con sugerencias al usuario por consola

--- Capturador.py
+++ Capturador.py
@@ -1,67 +1,143 @@
 from pynput import mouse,keyboard
-import threading
-from time import time
-from Archivo import escribir_accion
-from Control_correo import actualizar_dia, enviar, reiniciar_dia
-from Correo import enviar_correo
+from threading import Thread
+import time
+from Nodo import Nodo
+
+n_raiz = Nodo(0, 0)  # Creacion del nodo Raiz
+n_actual = n_raiz
+d_nodos = {"root": n_raiz}  # creacion de diccionario de nodos
+secuencia = []  # Lista con la secuencia de elementos
+conteoMax = 0  # la mayor cantidad de veces que se ha visitado un
+d_secuencias = {}
+l_ignoradas=[]
+l_secuencias = []
+t_inicial = time.time()
+lista=[]
+
+
+def crear_rama(linea, n_actual):
+    global conteoMax
+    tiempo  = float(linea[0])
+    informacion=tuple(linea[1:])
+    n_nuevo = Nodo(tiempo, informacion)  # creacion del nodo nuevo
+    id_nodo = informacion
+
+    if id_nodo in d_nodos:  # busqueda de nodo existente
+        n_existente = d_nodos[id_nodo]
+        t_registrado=n_existente.getTiempo()
+        t_actual=n_nuevo.getTiempo()
+        if t_actual>t_registrado and t_actual<1:
+            n_existente.setTiempo(t_actual)
+        n_actual.siguiente_nodo(n_existente)  # enlace de nodo existete
+        n_actual = n_existente
+
+        n_actual.cuenta()  # Al existir el nodo, se lleva un conteo de las veces que ha sido usado
+        if n_actual.getCuenta() >= conteoMax:
+            conteoMax = n_actual.getCuenta()  # actualizacion del conteoMaximo
+        # extraccion de la secuencia
+        if (conteoMax >= 100):
+           extraccion_secuencia(n_actual)
+    else:
+        d_nodos[id_nodo] = n_nuevo
+        n_actual.siguiente_nodo(n_nuevo)  # enlace de nodo nuevo
+        n_actual = n_nuevo
+    return n_actual
+
+
+def extraccion_secuencia(n_actual):
+    global conteoMax, l_secuencias, secuencia, d_secuencias, l_ignoradas
+
+    #print("conteo del nodo: ", n_actual.getCuenta())
+    #print("Conteo Max: ", conteoMax)
+
+    if ((100 * 0.7) < n_actual.getCuenta()) and (not(n_actual in secuencia)):
+        secuencia.append(n_actual)# creacion de la secuencia
+    else:
+        #Almacenar la secuencia
+        if (not(secuencia in l_secuencias)) and len(secuencia)>1 and (not(secuencia in l_ignoradas)):
+
+            print(secuencia)
+            for e in secuencia:
+                print(e.getInformacion())
+
+            print("Que haces?(i)gnorar  (Escribe el nombre para guardar)")
+            inp = str(input(">>"))
+
+            if inp != "i":
+                l_secuencias.append(secuencia)
+                d_secuencias[inp]=secuencia
+            else:
+                l_ignoradas.append(secuencia)
+        secuencia = []
+
+
+def preprocesamiento():
+    global n_actual, lista
+    while True:
+        print(len(lista))
+        for elemento in lista:
+            n_actual=crear_rama(elemento,n_actual)
+        lista=[]
+        time.sleep(30)
+
+
+def tiempo(t_final):
+    global lista
+    global t_inicial
+    t_total = round(t_final - t_inicial, 2)
+    t_inicial=time.time()
+    return t_total
+
 
 def on_move(x, y):
+    global lista
     colocacion = ('{0},{1}'.format(x, y))
-    escribir_accion(time(), "Mouse", "Moved", colocacion)
+    lista.append((tiempo(time.time()), "Mouse", "Moved", colocacion))
+
 
 def on_click(x, y, button, pressed):
+    global lista
     accion="{0}".format('Pressed' if pressed else 'Released')
-    escribir_accion(time(), "Mouse", accion, str(button))
+    lista.append((tiempo(time.time()), "Mouse", accion, str(button)))
+
 
 def on_scroll(x, y, dx, dy):
+    global lista
     colocacion=('{0}'.format('Down' if dy < 0 else 'Up'))
-    escribir_accion(time(), "Mouse", "Scrolled", colocacion)
+    lista.append((tiempo(time.time()), "Mouse", "Scrolled", colocacion))
+
 
 def on_press(key):
+    global lista
     try:
-        escribir_accion(time(), "Keyboard", "Pressed", key.char)
+        lista.append((tiempo(time.time()), "Keyboard", "Pressed", key.char))
     except AttributeError:
-        escribir_accion(time(), "Keyboard", "Pressed", str(key))
+        lista.append((tiempo(time.time()), "Keyboard", "Pressed", str(key)))
     except TypeError:
         pass
+
 
 def on_release(key):
+    global lista
     try:
-        escribir_accion(time(), "Keyboard", "Release", key.char)
+        lista.append((tiempo(time.time()), "Keyboard", "Release", key.char))
     except AttributeError:
-        escribir_accion(time(), "Keyboard", "Release", str(key))
+        lista.append((tiempo(time.time()), "Keyboard", "Release", str(key)))
     except TypeError:
         pass
 
-if enviar():
-    try:
-        enviar_correo()
-        actualizar_dia()
-        n_archivo = "C:\Capturador\lista_acciones.txt"
-        archivo = open(n_archivo, "w")
-        archivo.close()
-    except:
-        reiniciar_dia()
-else:
-    pass
 
 listener_keyboard= keyboard.Listener(
         on_press=on_press,
         on_release=on_release)
-listener_keyboard.setDaemon(True)
 
 listener_mouse= mouse.Listener(
         on_move=on_move,
         on_click=on_click,
         on_scroll=on_scroll)
-listener_mouse.setDaemon(True)
 
+p = Thread(target=preprocesamiento)
+
+p.start()
 listener_keyboard.start()
 listener_mouse.start()
-
-hilo_ppal = threading.main_thread() # Obtiene hilo principal
-
-for hilo in threading.enumerate(): # Recorre hilos activos para controlar estado de su ejecución
-    if hilo is hilo_ppal:    # Si el hilo es hilo_ppal continua al siguiente hilo activo
-        continue
-    hilo.join()         # El programa esperará a que este hilo finalice
